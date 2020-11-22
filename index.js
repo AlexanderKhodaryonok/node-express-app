2083372cee7f52e4931ac190eb8c27a84f6c453d
AlexanderKhodaryonok <alexanderkhodaryonok@gmail.com>
2020-11-23 01:40:40 +0300

    add-auth/add env vars

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express =  require('express');
-const userRoutes = require('./routes/users');
+const express = require("express");
+const userRoutes = require("./routes/users");
+const { PORT } = require("./config");
 
-const port = process.env.PORT || 3000;
+const port = PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
 
 app.listen(port, () => {
-    console.log(`Server API was started! Reserved port is ${port}`);
+  console.log(`Server API was started! Reserved port is ${port}`);
 });
 
 module.exports = app;

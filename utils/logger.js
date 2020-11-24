9e476852d0c7f49bce9076f084ad0950fdc00b03
AlexanderKhodaryonok <alexanderkhodaryonok@gmail.com>
2020-11-24 13:33:00 +0300

    add-auth/refactored logs styles

--- utils/logger.js
+++ utils/logger.js
@@ -1,25 +1,16 @@
 const { createLogger, format, transports } = require('winston');
-const { printf, combine, json } = format;
-
-const myFormat = printf((params) => {
-  return params.message;
-});
+const { printf, combine, json, colorize, simple } = format;
 
 const logger = createLogger({
   level: 'debug',
-  format: combine(
-    myFormat,
-  ),
   transports: [
     new transports.File({
       filename: 'logs/combined.log',
       level: 'info',
-      format: json(),
     }),
     new transports.File({
       filename: 'logs/errors.log',
       level: 'error',
-      format: json(),
     }),
   ]
 });
@@ -27,7 +18,10 @@ const logger = createLogger({
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new transports.Console({
     level: 'debug',
-    format: json(),
+    format: combine(
+      colorize({ all: true }),
+      simple(),
+    ),
   }));
 }
 
